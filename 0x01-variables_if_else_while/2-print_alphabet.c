a52e7d46bd7a5a05fb1689a15e5c5bf736a182ea
Drewjoe <andrewjose07@gmail.com>
2022-03-16 04:39:02 -0700

    A program that prints _putchar, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,15 @@
 #include <time.h>
 
 /**
- *
- * * main - This prints all the alphabet in lower case
- * *
- * * Return: Always 0 (Success)
- */
+ *	* main - This prints all the alphabet in lower case
+ *	*
+ *	* Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
 	char low_alphabet;
+
 	for (low_alphabet = 'a'; low_alphabet <= 'z'; low_alphabet++)
 	{
 		putchar(low_alphabet);
