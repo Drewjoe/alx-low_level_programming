f9b1f2860ece27e94e6f71abc2428a5cec35d770
Drewjoe <andrewjose711@gmail.com>
2022-03-16 15:31:36 +0100

    Prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+/** This is a main header file
+*
+* main: codes
+* Description: Main header file contains different function arguments
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
