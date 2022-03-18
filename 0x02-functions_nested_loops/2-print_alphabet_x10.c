b4bad63d687e7bc1536705d5f68478a9db97c8d1
Drewjoe <andrewjose711@gmail.com>
2022-03-18 08:53:07 +0100

    A function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 
 /**
  * Main: Print_alphabet_x10
+ * Desription: Prints 10 times the alphabet 10 timeusing _putchar
  * Function: Prints 10 times the alphabet 10 timeusing _putchar
- * @my_arg: void
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
