595a4f8c6f2aed8bc26eb20cd186b4b977cb8ffe
Drewjoe <andrewjose07@gmail.com>
2022-03-31 03:14:39 -0700

    Header file for 0x08-recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+/** This is a header file containing prototypes for
+ * 0x08-recursion task
+ */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char c);
+
