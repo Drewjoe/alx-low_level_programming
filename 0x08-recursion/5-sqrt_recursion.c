9eb7d276c3281903dbe39603e58464ed2121630a
Drewjoe <andrewjose07@gmail.com>
2022-03-31 05:12:14 -0700

    Returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int check(int a, int b)
 		return (a);
 	if (a * a > b)
 		return (-1);
-	return (sqrt_check(a + 1, b));
+	return (check(a + 1, b));
 }
 
 /**
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (sqrt_check(1, n));
+	return (check(1, n));
 }
