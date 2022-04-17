e8cf864c66cdcbd34a268a539ed7ba4494834e27
Drewjoe <andrewjose07@gmail.com>
2022-04-17 12:16:27 -0700

    Function to print anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,43 +4,23 @@
 #include <stdlib.h>
 
 /**
- * print_char - prints a character
- * @list: char to print
+ * print_c - print a char
+ * @c: char to print
  * Return: void
  */
-void print_char(va_list list)
+void print_c(va_list c)
 {
-	printf("%c", va_arg(list, int));
+	printf("%c", va_arg(c, int));
 }
 
 /**
- * print_int - prints an integer
- * @list: integer to print
+ * print_s - prints a string
+ * @s: string to print
  * Return: void
  */
-void print_int(va_list list)
+void print_s(va_list s)
 {
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * print_float - prints a float
- * @list: float to print
- * Return: void
- */
-void print_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-
-/**
- * print_str - prints a string
- * @list: string to print
- * Return: void
- */
-void print_str(va_list list)
-{
-	char *str = va_arg(list, char *);
+	char *str = va_arg(s, char *);
 
 	if (str == NULL)
 		str = "(nil)";
@@ -48,24 +28,45 @@ void print_str(va_list list)
 }
 
 /**
+ * print_i - prints an int
+ * @i: int to print
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
  * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * @format: list of argument types passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
+
 	print_t p[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_str},
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
 		{NULL, NULL}
 	};
-	va_list list;
+	va_list valist;
 	char *separator = "";
 
-	va_start(list, format);
+	va_start(valist, format);
 	i = 0;
 	while (format && format[i])
 	{
@@ -75,14 +76,14 @@ void print_all(const char * const format, ...)
 			if (*(p[j].t) == format[i])
 			{
 				printf("%s", separator);
-				p[j].f(list);
+				p[j].f(valist);
 				separator = ", ";
 				break;
 			}
-			j++
+			j++;
 		}
-		i++
+		i++;
 	}
+	va_end(valist);
 	printf("\n");
-	va_end(list);
 }
