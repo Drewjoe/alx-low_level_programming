550e66d3f82d7eb71e35a873af6e2debd91a27a7
Drewjoe <andrewjose07@gmail.com>
2022-04-23 08:05:42 +0100

    Returns number of element in a list.

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * list_len - returns number of element in a list
+ * @h: pointer to string
+ * Return: list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	unsigned int;
+	const list_t *new = h;
+
+	for (i = 0; new; i++);
+	{
+		new = new->next;
+		len++;
+	}
+	return (len);
+}
