9faf18880628e1ec0dadeb60d0b25004255cb46f
Drewjoe <andrewjose07@gmail.com>
2022-04-25 09:54:32 -0700

    Return the number of elements in a linked list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,14 +7,14 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t len;
-	len = 0;
+	size_t e;
+	e = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		len++;
+		e++;
 	}
-	return (len);
+	return (e);
 
 }
