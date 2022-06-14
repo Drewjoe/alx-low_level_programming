45a70125651c4d92f06609b784ba88358e7bf299
Drewjoe <andrewjose711@gmail.com>
2022-06-14 16:02:23 +0100

    Adds a new node at the end of a dlistint_t list.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,34 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - add a node at the end of a doubly-linked list
- * @head: pointer to a pointer to the beginning of a doubly-linked list
- * @n: the data to add
- * Return: If memory allocation fails or head is NULL, return NULL.
- * Otherwise, return a pointer to the new node.
+ * add_dnodeint_end - add element at the end of the list
+ * @head: pointer to pointer of list
+ * @n:element in list
+ * Return:new element or NULL if failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *tail, *new = NULL;
+	dlistint_t *new, *start;
 
-	if (head)
+	start = *head;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
 	{
-		new = malloc(sizeof(*new));
-		if (new)
-		{
-			new->n = n;
-			new->next = NULL;
-
-			tail = *head;
-			if (tail)
-			{
-				while (tail->next)
-					tail = tail->next;
-
-				new->prev = tail;
-				tail->next = new;
-			}
-			else
-			{
-				new->prev = NULL;
-				*head = new;
-			}
-		}
+		new->next = NULL;
+		new->n = n;
+		new->prev = NULL;
+		*head = new;
+		return (new);
 	}
+	while (start->next)
+	{
+		start = start->next;
+	}
+	start->next = new;
+	new->prev = start;
+	new->next = NULL;
+	new->n = n;
 	return (new);
 }
